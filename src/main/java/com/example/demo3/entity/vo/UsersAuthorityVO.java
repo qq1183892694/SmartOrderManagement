25ac01c096880c1fcf9c03d4026c4ef3faa07d8e
Frankyyyyy <1183892694@qq.com>
2020-07-31 21:41:24 +0800

    范英齐于2020年7月31日21:41提交

---
+++ src/main/java/com/example/demo3/entity/vo/UsersAuthorityVO.java
@@ -0,0 +1,82 @@
+package com.example.demo3.entity.vo;
+
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.annotations.TableId;
+
+import java.io.Serializable;
+
+public class UsersAuthorityVO implements Serializable {
+    @TableId("uid")
+    private Long uid;
+    @TableField("userLoginName")
+    private String userLoginName;
+    @TableField("userPassword")
+    private String userPassword;
+    @TableField("userAvatarPath")
+    private String userAvatarPath;
+    @TableField("sysName")
+    private String sysName;
+    @TableField("characterName")
+    private String characterName;
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long userId) {
+        this.uid = userId;
+    }
+
+    public String getUserLoginName() {
+        return userLoginName;
+    }
+
+    public void setUserLoginName(String userLoginName) {
+        this.userLoginName = userLoginName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+
+    public String getUserAvatarPath() {
+        return userAvatarPath;
+    }
+
+    public void setUserAvatarPath(String userAvatarPath) {
+        this.userAvatarPath = userAvatarPath;
+    }
+
+    public String getSysName() {
+        return sysName;
+    }
+
+    public void setSysName(String sysName) {
+        this.sysName = sysName;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    @Override
+    public String toString() {
+        return "UsersAuthorityVO{" +
+                "userId=" + uid +
+                ", userLoginName='" + userLoginName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", userAvatarPath='" + userAvatarPath + '\'' +
+                ", sysName='" + sysName + '\'' +
+                ", characterName='" + characterName + '\'' +
+                '}';
+    }
+}
